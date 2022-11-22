b3d6b15a799306518ebd3ba4edfcab853b9a4540
James Braza <jamesbraza@gmail.com>
2022-11-21 21:55:08 -0800

    Moved td_pre_process_weight to embedding

--- embedding/td_pre_process_weight.py
+++ embedding/td_pre_process_weight.py
@@ -3,48 +3,29 @@ import os
 import numpy as np
 import tensorflow as tf
 
+from embedding.embed import embed_model
 from models.core import TransferModel
 
 learning_rate = 0.001
 
 
-def get_weight_matrix_input(fine_tune_weights_list: list[str]):
+def get_weight_matrix_input(fine_tune_weights_list: list[str]) -> np.ndarray:
     weigth_matrix = []
     for fine_tune_weights in fine_tune_weights_list:
         print(fine_tune_weights)
-        # ======model weight matrxi embedding:
+        # ======model weight matrix embedding:
         base_model = TransferModel()
         checkpoint = tf.train.Checkpoint(base_model)
         checkpoint.restore(fine_tune_weights).expect_partial()
 
-        opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
         base_model.compile(
-            optimizer=opt,
+            optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
             loss="categorical_crossentropy",
             metrics=["categorical_accuracy"],
         )
         base_model.build(input_shape=(1, 28, 28, 3))
-        # base_model.summary()
 
-        # ======just use last conv layer
-        for layer in base_model.layers[2:3]:
-            # print(layer.name)
-            weights = layer.get_weights()[0]
-            weigth_np = np.array(weights, dtype=object)
-            # print(weigth_np.shape)
-            weigth_np_b = np.swapaxes(weigth_np, 3, 0)
-            # print(weigth_np_b.shape)
-            weigth_np_r = np.reshape(
-                weigth_np_b,
-                (
-                    weigth_np_b.shape[0],
-                    weigth_np_b.shape[1] * weigth_np_b.shape[2] * weigth_np_b.shape[3],
-                ),
-            )
-            # print(weigth_np_r.shape)
-            # weigth_np_flat=weigth_np.flatten()
-            # print(weigth_np_flat.shape)
-
+        weigth_np_r = embed_model(base_model)
         weigth_matrix.append(weigth_np_r)
     weigth_matrix_np = np.array(weigth_matrix, dtype=object).astype("float32")
     print(">>>>>final shape", weigth_matrix_np.shape)
