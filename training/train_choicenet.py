4bd7d6e6c6e93139d32226a07879a88d788165fa
James Braza <jamesbraza@gmail.com>
2022-11-24 15:49:59 -0500

    Added batch_size arg, validation split, and model prediction

--- training/train_choicenet.py
+++ training/train_choicenet.py
@@ -7,7 +7,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-from data.dataset import DATASET_CONFIGS, DEFAULT_SEED
+from data.dataset import DATASET_CONFIGS, DEFAULT_BATCH_SIZE, DEFAULT_SEED
 from embedding.embed import embed_dataset, embed_model
 from models.core import ChoiceNetSimple, TransferModel
 from training import LOG_DIR
@@ -47,6 +47,11 @@ class TLDataset(tf.keras.utils.Sequence):
         bslice = slice(idx * self.batch_size, (idx + 1) * self.batch_size)
         return (self.arrays[0][bslice], self.arrays[1][bslice]), self.arrays[2][bslice]
 
+    def get_accuracies(self, idx: int) -> np.ndarray:
+        """Get only the dataset's accuracies for the input batch index."""
+        bslice = slice(idx * self.batch_size, (idx + 1) * self.batch_size)
+        return self.arrays[2][bslice]
+
 
 def train(args: argparse.Namespace) -> None:
     tf.random.set_seed(args.seed)
@@ -79,8 +84,16 @@ def train(args: argparse.Namespace) -> None:
         loss="mse",
         metrics=["mse"],
     )
-    sequence = TLDataset(tlds)  # TODO: make batch_size an arg
-    model.fit(sequence)
+
+    num_training_ds = int(len(tlds) * (1 - args.validation_split))
+    training_dataset = TLDataset(tlds[:num_training_ds], batch_size=args.batch_size)
+    test_dataset = TLDataset(tlds[num_training_ds:], batch_size=args.batch_size)
+
+    model.fit(training_dataset)
+    preds: np.ndarray = model.predict(test_dataset)
+    accuracies: list[np.ndarray] = [
+        test_dataset.get_accuracies(i) for i in range(len(test_dataset))
+    ]
     _ = 0  # Debug here
 
 
@@ -103,6 +116,18 @@ def main() -> None:
     )
     parser.add_argument(
         "--learning_rate", type=float, default=1e-3, help="learning rate"
+    )
+    parser.add_argument(
+        "--validation_split",
+        type=float,
+        default=0.3,
+        help="fraction of transfer learning datasets to use for validation",
+    )
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=DEFAULT_BATCH_SIZE,
+        help="batch size for training and prediction",
     )
     parser.add_argument(
         "--log_dir", type=str, default=LOG_DIR, help="log base directory"
