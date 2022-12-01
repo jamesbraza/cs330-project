f9ae606b730962f02bbb6beeae4fc19d920e8e96
James Braza <jamesbraza@gmail.com>
2022-11-30 20:43:49 -0800

    Added tlds dir to gitignore

--- training/__init__.py
+++ training/__init__.py
@@ -1,5 +1,6 @@
 import os
 
-# Directory where logs (e.g. TensorBoard) will be placed
 TRAINING_DIR = os.path.dirname(__file__)
+# Directory where logs (e.g. TensorBoard) will be placed
 LOG_DIR = os.path.join(TRAINING_DIR, "logs")
+TLDS_DIR = os.path.join(TRAINING_DIR, "tlds")
