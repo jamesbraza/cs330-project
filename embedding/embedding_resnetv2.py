d97800fe00233ab0d8a7fe3f2ae2bfd23b268f0c
CDC1688 <46419711+CDC1688@users.noreply.github.com>
2022-12-13 14:57:37 +0800

    Added anna's work from anna branch (#33)
    Co-authored-by: James Braza <jamesbraza@gmail.com>

---
+++ embedding/embedding_resnetv2.py
@@ -0,0 +1,48 @@
+import numpy as np
+import tensorflow as tf
+
+
+def embedding_resnet(X_PATH, Y_PATH):
+    # ====define model
+    X_input = tf.keras.Input(shape=(64, 64, 3))
+    output = tf.keras.applications.ResNet50V2(
+        weights="imagenet", pooling=None, include_top=False
+    )(X_input)
+    model = tf.keras.Model(inputs=X_input, outputs=output)
+    model.summary()
+
+    # ====load dataset
+    X_TEST = np.load(X_PATH)
+    Y_TEST = np.load(Y_PATH)
+
+    X_TEST = tf.cast(X_TEST, tf.float32)
+    Y_TEST = tf.cast(Y_TEST, tf.float32)
+
+    embedding_resnet = model.predict(X_TEST)
+    print(embedding_resnet.shape)
+    embedding_resnet_r = tf.reshape(
+        embedding_resnet, shape=(embedding_resnet.shape[0], -1)
+    )
+    Y_TEST = tf.expand_dims(Y_TEST, 1)
+    embedding_final = tf.concat([Y_TEST, embedding_resnet_r], axis=1)
+    index = tf.squeeze(embedding_final[:, 0:1])
+    index = tf.cast(index, tf.int32)
+    num_class = len(np.unique(index))
+    class_mean = tf.math.unsorted_segment_mean(embedding_final, index, num_class)
+    print(class_mean.shape)
+    return class_mean
+
+
+# =====cat tune and the TL dataset embedding
+if __name__ == "__main__":
+    X_PATH = "/content/drive/My Drive/cs330_dataset/x1_64.npy"
+    Y_PATH = "/content/drive/My Drive/cs330_dataset/y1_64.npy"
+    x_tune = np.load(
+        "/content/drive/My Drive/cs330_dataset/x_tune_64_all_embedding_class_mean.npy"
+    )
+    class_mean_x = embedding_resnet(X_PATH, Y_PATH)
+    out = tf.concat([class_mean_x, x_tune], axis=0)
+    print(out.shape)
+    np.save(
+        "/content/drive/My Drive/cs330_dataset/x1_64_all_embedding_class_mean.npy", out
+    )
---
+++ training/td_train_embed.py
@@ -0,0 +1,145 @@
+import os
+import random
+import sys
+from argparse import ArgumentParser
+
+import numpy as np
+import tensorflow as tf
+
+from models.core import Model_ChoiceNet_emb, TransferModel
+
+learning_rate = 0.001
+
+
+def get_weight_matrix_input(fine_tune_weights_list):
+    weigth_matrix = []
+    for fine_tune_weights in fine_tune_weights_list:
+        print(fine_tune_weights)
+
+        # ======model weight matrxi embedding:
+        base_model = TransferModel()
+        checkpoint = tf.train.Checkpoint(base_model)
+        checkpoint.restore(fine_tune_weights).expect_partial()
+
+        opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        base_model.compile(
+            optimizer=opt,
+            loss="categorical_crossentropy",
+            metrics=["categorical_accuracy"],
+        )
+        base_model.build(input_shape=(None, 28, 28, 3))
+        # base_model.summary()
+
+        # ======just use last conv layer
+        for layer in base_model.layers[2:3]:
+            # print(layer.name)
+            weights = layer.get_weights()[0]
+            weigth_np = np.array(weights, dtype=object)
+            weigth_np_flat = weigth_np.flatten()
+            # print(weigth_np_flat.shape)
+
+        weigth_matrix.append(weigth_np_flat)
+    weigth_matrix_np = np.array(weigth_matrix, dtype=object).astype("float32")
+    # print(weigth_matrix_np.shape)
+    return weigth_matrix_np
+
+
+def train_model(args):
+    learning_rate = args.learning_rate
+    max_epoch = args.maxEpoch
+    batch_size = args.batch_size
+    weight_matrix_path = args.weight_matrix_path
+    y_matrix = args.y_matrix
+    log_dir = args.log_dir
+
+    X_weight = np.load(weight_matrix_path)
+    Y_matrix = np.load(y_matrix)
+
+    X_weight = np.array(X_weight).astype(np.float32)
+    Y_matrix = np.array(Y_matrix).astype(np.float32)
+
+    print(X_weight.shape)
+    print(Y_matrix.shape)
+
+    X_train = X_weight[0:-1]
+    Y_train = Y_matrix[0:-1]
+
+    X_val = X_weight[-1:]
+    Y_val = Y_matrix[-1:]
+
+    model = Model_ChoiceNet_emb()
+
+    opt = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    model.compile(
+        optimizer=opt, loss="mse", metrics=[tf.keras.metrics.MeanSquaredError()]
+    )
+    model.build(input_shape=(None, 18, 8193))
+    # model = Model(inputs=[base_model.input,base_model2.input], outputs=output)
+    model.summary()
+
+    model_save_callback = tf.keras.callbacks.ModelCheckpoint(
+        log_dir + "/weights/", save_best_only=True, save_weights_only=False
+    )
+    train_log_callback = tf.keras.callbacks.CSVLogger(
+        log_dir + "training.csv", separator=","
+    )
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        log_dir=log_dir, histogram_freq=1
+    )
+
+    train_model = model.fit(
+        X_train,
+        Y_train,
+        epochs=max_epoch,
+        batch_size=1,
+        validation_data=(X_val, Y_val),
+        callbacks=[
+            model_save_callback,
+            train_log_callback,
+        ],
+        verbose=1,
+        shuffle=True,
+    )
+
+
+def main():
+    parser = ArgumentParser(description="Train parameters ")
+
+    parser.add_argument(
+        "--learning_rate", type=float, default=1e-3, help="Set learning rate"
+    )
+
+    parser.add_argument("--maxEpoch", type=int, default=30, help="Maximum epochs")
+
+    parser.add_argument(
+        "--batch_size", type=int, default=1, help="Set the batch size for training"
+    )
+
+    parser.add_argument(
+        "--weight_matrix_path",
+        type=str,
+        default="/data1/cs330/project/res_net_embedding/emb_resnet_x_all.npy",
+        help="path for the reducd weight matrix",
+    )
+
+    parser.add_argument(
+        "--y_matrix",
+        type=str,
+        default="/data1/cs330/project/data/x_feature/y_train.npy",
+        help="path for the accuracy from fine-tuning val",
+    )
+
+    parser.add_argument(
+        "--log_dir",
+        type=str,
+        default="/data1/cs330/project/test_tdl_weight_only",
+        help="log",
+    )
+
+    args = parser.parse_args()
+
+    train_model(args)
+
+
+if __name__ == "__main__":
+    main()
